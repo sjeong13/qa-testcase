7279d1cca5785d1d3d4cd9731e85626b6aee14ab
sjeong13 <im.sj@imweb.me>
2025-11-14 10:44:18 +0900

    Update qa-testcase.py

--- qa-testcase.py
+++ qa-testcase.py
@@ -362,6 +362,8 @@ with st.sidebar:
         st.markdown("<br>", unsafe_allow_html=True)
         st.markdown("<br>", unsafe_allow_html=True)
         st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
         
         # 테스트 케이스 요약
         st.subheader(f"📋 저장된 테스트 케이스")
